241c82a93e33c6adda60c70c3b40384967cf3ebf
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-09-10 15:45:09 -0400

    added Mentor, Student, and Quiz models

---
+++ server/models/Student.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const studentSchema = new Schema(
+  {
+    firstName: String,
+    lastName: String,
+    email: String,
+    phoneNumber: {
+      type: Number,
+      required: true},
+    zipCode: {
+      type: Number,
+      required: true},
+    imageUrl: String,
+    interests: Array,
+    languagesSpoken: Array,
+    quizzesTaken: [{ type: Schema.Types.ObjectId, ref: "Quiz" }],
+    scoreAverage: Number,
+    mentors: [{ type: Schema.Types.ObjectId, ref: "Mentor" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Student", mentorSchema);
